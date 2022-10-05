583431f77779ba80d56c0aa2783708875c2e84ca
geleiadementa <geleiadementa@tuta.io>
2022-10-05 18:08:01 -0300

    add make samples

---
+++ prepare_data/make_samples.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from os import getenv
+
+dataset_base_path = getenv('dataset_base')
+n_samples = int(getenv('n_amostras'))
+seed = int(getenv('seed'))
+dataset_sample = getenv('dataset_sample')
+
+(
+    pd.read_csv(dataset_base_path, delimiter="\t")
+    .groupby('ethnicity')
+    .sample(n_samples, random_state=seed)
+    .to_csv(dataset_sample, sep="\t")
+)
