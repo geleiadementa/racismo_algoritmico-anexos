eba39245ffd0264898947e6bead085ad0bf191a1
geleiadementa <geleiadementa@tuta.io>
2022-11-05 07:28:55 -0300

    add 'gender'

--- prepare_data/make_base.py
+++ prepare_data/make_base.py
@@ -5,34 +5,43 @@ from os import getenv
 from tqdm import tqdm
 from glob import glob
 
-labels_file = getenv('labels')
-raw_images_path = getenv('raw_images')
-dataset_output = getenv('dataset_base')
+labels_file = getenv("labels")
+identity_meta = getenv("identity_meta")
+raw_images_path = getenv("raw_images")
+dataset_output = getenv("dataset_base")
 
 regex = re.compile(r"(n\d{6})")
 
-if __name__ == '__main__':
 
-    with open(labels_file) as f:
-        labels = xmltodict.parse(f.read())['xml']['subject']
+def make_index_dict(content: iter, id_col: str, target: str) -> dict:
+    _t = {}
+    for i in content:
+        _t[i[id_col]] = i[target]
 
-    _id2ethnicity = {}
+    return _t
 
-    for i in labels:
-        _id2ethnicity[i['id']] = i['ethnicity']
+
+if __name__ == "__main__":
+
+    with open(labels_file) as f0:
+        labels = xmltodict.parse(f0.read())["xml"]["subject"]
+        id2ethnicity = make_index_dict(labels, "id", "ethnicity")
+
+    with open(identity_meta, newline="") as f1:
+        identity_dict = csv.DictReader(f1, skipinitialspace=True)
+        id2identity = make_index_dict(identity_dict, "Class_ID", "Gender")
 
     files = glob(raw_images_path + "/*/*.jpg")
 
-    with open(dataset_output, 'w', newline='') as f:
-        dataset_writer = csv.writer(
-            f, delimiter='\t'
-        )
+    with open(dataset_output, "w", newline="") as f:
+        dataset_writer = csv.writer(f, delimiter="\t")
         # write header
-        dataset_writer.writerow(['id', 'filepath', 'ethnicity'])
+        dataset_writer.writerow(["id", "filepath", "ethnicity", "gender"])
 
-        for filepath in tqdm(files, ncols=80, unit='files', mininterval=0.5):
+        for filepath in tqdm(files, ncols=80, unit="files", mininterval=0.5):
             match = regex.search(filepath)
             _id = match.group()
-            ethnicity = _id2ethnicity[_id]
+            ethnicity = id2ethnicity[_id]
+            gender = id2identity[_id]
 
-            dataset_writer.writerow([_id, filepath, ethnicity])
+            dataset_writer.writerow([_id, filepath, ethnicity, gender])
