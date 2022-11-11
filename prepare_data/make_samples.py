f9a83cfca22c5cf85e05e92dc1ecbd1154bbc5cd
geleiadementa <geleiadementa@tuta.io>
2022-11-11 10:40:55 -0300

    update method [necessary change pandas -> polars]

--- prepare_data/make_samples.py
+++ prepare_data/make_samples.py
@@ -1,14 +1,26 @@
-import pandas as pd
+import polars as pl
 from os import getenv
 
-dataset_base_path = getenv('dataset_base')
-n_samples = int(getenv('n_amostras'))
-seed = int(getenv('seed'))
-dataset_sample = getenv('dataset_sample')
+dataset_base_path = getenv("dataset_base")
+n_samples = int(getenv("n_amostras"))
+seed = int(getenv("seed"))
+dataset_sample = getenv("dataset_sample")
+
+df = pl.read_csv(dataset_base_path, sep="\t")
+
+ids = (
+    df.groupby(["ethnicity", "gender"])
+    .agg([pl.col("id").unique().shuffle().head(n=n_samples)])
+    .explode(pl.col("id"))
+    .sort(["ethnicity", "gender"])["id"]
+)
+
+mask = pl.all().exclude("id")
 
 (
-    pd.read_csv(dataset_base_path, delimiter="\t")
-    .groupby(['ethnicity', 'gender'])
-    .sample(n_samples, random_state=seed)
-    .to_csv(dataset_sample, sep="\t", index=False)
+    df.filter(pl.col("id").is_in(ids))
+    .groupby(pl.col("id"))
+    .agg(mask.shuffle().head(n=n_samples))
+    .explode(mask)
+    .write_csv(dataset_sample, sep="\t")
 )
